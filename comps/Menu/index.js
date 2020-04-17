e6ab72e6ac952af7eb1ceedcd52669093413b502
=CMDOCH <=christine.docherty96@gmail.com>
2020-04-16 21:50:22 -0800

    Need help with menu Dx

--- comps/Menu/index.js
+++ comps/Menu/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './menu.css';
 
-const Menu = () => <div>
+const backImg = require('./SVG/Back_Arrow.svg');
+const logoImg = require('./SVG/Shark_Logo.svg');
+const hamImg = require('./SVG/Hamburger_Menu.svg');
 
+
+const Menu = ({backimg, logoimg, hamimg}) => <div className='menu'>
+    <div className='menu_icons'>
+        <div idName="iconback">
+        <img className="icon" src={backimg} />
+        </div>
+        <div idName="iconlogo">
+        <img className="icon" src={logoimg} />
+        </div>
+        <div idName="iconham">
+        <img className="icon" src={hamimg} />
+        </div>
+    </div>
 </div>
 
-Menu.defaultProps = {
-
-}
+    Menu.defaultProps = {
+        backimg: backImg,
+        logoimg: logoImg,
+        hamimg: hamImg
+    }
 
 export default Menu;
